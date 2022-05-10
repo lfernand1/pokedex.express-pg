07532859646ed7f694b61707d91baa81a0a99962
Larissa Fernanda <larifernand7@gmail.com>
2022-05-10 13:59:22 +0044

    ssl

--- src/database/db.js
+++ src/database/db.js
@@ -3,10 +3,7 @@ const Sequelize = require("sequelize");
 const connection = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   dialectOpitions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
+    ssl: true,
   },
 });
 
