6d2eadeca3a2ce9bbe3e21252d8413223258e37a
Larissa Fernanda <larifernand7@gmail.com>
2022-05-10 21:42:12 +0044

    222

--- src/database/db.js
+++ src/database/db.js
@@ -1,13 +1,19 @@
 const Sequelize = require("sequelize");
 
-const connection = new Sequelize("postgres://admin:JX1fyHEXQZgTils6WGaBCRptDVwQB0Xs@dpg-c9tfhmn9re0h93t4vgm0-a.oregon-postgres.render.com/pokemon_bgxz", {
-  dialect: "postgres",
-  dialectOpitions: {  ssl: {
-    require: true,
-    rejectUnauthorized: false
-}
-   
+const connection = new Sequelize(
+  "postgres://admin:JX1fyHEXQZgTils6WGaBCRptDVwQB0Xs@dpg-c9tfhmn9re0h93t4vgm0-a.oregon-postgres.render.com/pokemon_bgxz",
+
+  {
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnautorized: false,
+      },
+    },
   }
-});
+);
 
 module.exports = connection;
+
+
